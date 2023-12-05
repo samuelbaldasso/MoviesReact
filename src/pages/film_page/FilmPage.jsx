86e423d80b4c148b6d5452443b445a4013f134bb
Samuel Baldasso <samuelbaldasso93@gmail.com>
2023-12-05 17:34:49 -0300

    hotfix: url das apis

--- src/pages/film_page/FilmPage.jsx
+++ src/pages/film_page/FilmPage.jsx
@@ -2,26 +2,19 @@ import Header from "../../components/header/Header";
 import Film from "../films/Film";
 import Line from "../../components/line/Line";
 import { useUser } from "../../UserContext";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import jwtDecode from "jwt-decode";
 import axios from "axios";
 import "./style.css";
 
 export default function FilmPage() {
-  const {
-    user,
-    setUser,
-    film,
-    setFilm,
-    setFilteredFilms,
-    filteredFilms,
-    setTag,
-    search,
-  } = useUser();
+  const { user, setUser, setFilteredFilms, setFilm, setTag, search } = useUser();
 
+  // Novo estado para armazenar os filmes obtidos do servidor
+  const [serverFilms, setServerFilms] = useState([]);
 
   useEffect(() => {
-    function createFilter(){
+    function createFilter() {
       const token = localStorage.getItem("token");
       let userId;
       try {
@@ -34,37 +27,47 @@ export default function FilmPage() {
         console.error("Erro ao decodificar o token.", err);
       }
     }
+    createFilter();
+  }, []);
 
-    async function handleFilms(e) {
+  useEffect(() => {
+    async function handleFilms() {
       try {
-        const response = await axios.get("https://movies-backend-nodejs-2.onrender.com/film/film");
+        const response = await axios.get(
+          "https://movies-backend-nodejs-2.onrender.com/film/film"
+        );
         const localId = localStorage.getItem("userId");
         const filteredData = response.data.filter(
           (film) => film.users_id === localId
         );
-        setFilm(filteredData);
-        if (search) {
-          const filteredDataFilms = film.filter((f) =>
-            f.title.toLowerCase().includes(search.toLowerCase())
-          );
-          setFilteredFilms(filteredDataFilms);
-        } else {
-          setFilteredFilms(film);
-        }
+        setServerFilms(filteredData); // Atualiza os filmes do servidor
       } catch (error) {
         alert("Erro ao adicionar filme.");
       }
     }
 
     handleFilms();
-    createFilter();
-  }, [setUser, setFilm, setFilteredFilms, filteredFilms, search]);
+  }, []); // Depende apenas do userId
+
+  useEffect(() => {
+    let filteredDataFilms = serverFilms;
+
+    if (search) {
+      filteredDataFilms = serverFilms.filter((f) =>
+        f.title.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+
+    setFilm(filteredDataFilms);
+  }, [search, serverFilms, setFilm]); // Agora depende de search e serverFilms
 
   useEffect(() => {
     async function getUser() {
       try {
         const localId = localStorage.getItem("userId");
-        const res = await axios.get(`https://movies-backend-nodejs-2.onrender.com/user/user/${localId}`);
+        const res = await axios.get(
+          `https://movies-backend-nodejs-2.onrender.com/user/user/${localId}`
+        );
         setUser(res.data);
       } catch (e) {
         alert("Erro ao obter usuário.");
@@ -82,7 +85,7 @@ export default function FilmPage() {
 
     // handleTags();
     getUser();
-  }, [user, setUser, setFilm, setTag, setFilteredFilms]);
+  }, [user, setUser, setTag, setFilteredFilms]);
 
   return (
     <>
